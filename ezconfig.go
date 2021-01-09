df1689d596f3c7adfc8d07a022c05e158d542223
gbaranski <gbaranski19@gmail.com>
2021-01-09 15:08:28 +0100

    style: reformat

--- ezconfig.go
+++ ezconfig.go
@@ -29,47 +29,46 @@ func main() {
 }
 */
 func Parse(v interface{}) error {
-  ptrv := reflect.ValueOf(v)
-  if ptrv.Kind() != reflect.Ptr || ptrv.IsNil() {
-    return  fmt.Errorf("invalid type, expected struct, received %s", ptrv.Kind().String())
-  }
-  ev := ptrv.Elem()
-  if ev.Kind() != reflect.Struct {
-    return fmt.Errorf("invalid type, expected *struct, received %s", ev.Type().Name())
+	ptrv := reflect.ValueOf(v)
+	if ptrv.Kind() != reflect.Ptr || ptrv.IsNil() {
+		return fmt.Errorf("invalid type, expected struct, received %s", ptrv.Kind().String())
+	}
+	ev := ptrv.Elem()
+	if ev.Kind() != reflect.Struct {
+		return fmt.Errorf("invalid type, expected *struct, received %s", ev.Type().Name())
 
-  }
-  for i := 0; i < ev.NumField(); i++ {
-    f := ev.Field(i)
-    tf := ev.Type().Field(i)
-    if !f.IsValid() {
-      return fmt.Errorf("field %d is not valid", i)
-    }
-    if !f.CanSet() {
-      return fmt.Errorf("field %d is not changeable", i)
-    }
-    tag, texists := tf.Tag.Lookup("env")
-    if !texists {
-      continue
-    }
-    env, eexists := os.LookupEnv(tag)
-    if !eexists {
-      return fmt.Errorf("ENV %s not found", tag)
-    }
+	}
+	for i := 0; i < ev.NumField(); i++ {
+		f := ev.Field(i)
+		tf := ev.Type().Field(i)
+		if !f.IsValid() {
+			return fmt.Errorf("field %d is not valid", i)
+		}
+		if !f.CanSet() {
+			return fmt.Errorf("field %d is not changeable", i)
+		}
+		tag, texists := tf.Tag.Lookup("env")
+		if !texists {
+			continue
+		}
+		env, eexists := os.LookupEnv(tag)
+		if !eexists {
+			return fmt.Errorf("ENV %s not found", tag)
+		}
 
-    switch f.Kind() {
-    case reflect.String:
-      f.SetString(env)
-    case reflect.Int:
-      intenv, err := strconv.Atoi(env)
-      if err != nil {
-        return fmt.Errorf("%s: %s", tf.Name, err.Error())
-      }
-      f.SetInt(int64(intenv))
-    default:
-      return fmt.Errorf("kind %s not supported", f.Kind().String())
+		switch f.Kind() {
+		case reflect.String:
+			f.SetString(env)
+		case reflect.Int:
+			intenv, err := strconv.Atoi(env)
+			if err != nil {
+				return fmt.Errorf("%s: %s", tf.Name, err.Error())
+			}
+			f.SetInt(int64(intenv))
+		default:
+			return fmt.Errorf("kind %s not supported", f.Kind().String())
 
-    }
-  }
-  return nil
+		}
+	}
+	return nil
 }
-
--- ezconfig_test.go
+++ ezconfig_test.go
@@ -7,26 +7,26 @@ import (
 )
 
 func TestParse(t *testing.T) {
-  type Enviroments struct {
-    SomeName    string `env:"someName"`
-    SomeInteger int `env:"someInteger"`
-  }
-  ex := Enviroments{
-    SomeName: "helloworld",
-    SomeInteger: 20,
-  }
-  os.Setenv("someName", ex.SomeName)
-  os.Setenv("someInteger", strconv.Itoa(ex.SomeInteger))
+	type Enviroments struct {
+		SomeName    string `env:"someName"`
+		SomeInteger int    `env:"someInteger"`
+	}
+	ex := Enviroments{
+		SomeName:    "helloworld",
+		SomeInteger: 20,
+	}
+	os.Setenv("someName", ex.SomeName)
+	os.Setenv("someInteger", strconv.Itoa(ex.SomeInteger))
 
-  var re Enviroments
-  err := Parse(&re)
-  if err != nil {
-    t.Fatalf(err.Error())
-  }
-  if re.SomeName != ex.SomeName {
-    t.Fatalf("'SomeName' mismatch, ex: %s, re: %s", ex.SomeName, re.SomeName)
-  }
-  if re.SomeInteger != ex.SomeInteger {
-    t.Fatalf("'SomeInteger' mismatch, ex: %d, re: %d", ex.SomeInteger, re.SomeInteger)
-  }
+	var re Enviroments
+	err := Parse(&re)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	if re.SomeName != ex.SomeName {
+		t.Fatalf("'SomeName' mismatch, ex: %s, re: %s", ex.SomeName, re.SomeName)
+	}
+	if re.SomeInteger != ex.SomeInteger {
+		t.Fatalf("'SomeInteger' mismatch, ex: %d, re: %d", ex.SomeInteger, re.SomeInteger)
+	}
 }
